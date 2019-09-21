a22b2d45df15cd66974b1591e08431b54b1ef3b6
Anton <kotovichAnt@mail.ru>
2019-09-21 14:29:28 +0300

    ArithmeticOperations

---
+++ ArithmeticOperations/ArithmeticOperations/F_ArithmeticOperations.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace ArithmeticOperations
+{
+    partial class F_ArithmeticOperations
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_ArithmeticOperations));
+            this.tb_number1 = new System.Windows.Forms.TextBox();
+            this.gB_interface = new System.Windows.Forms.GroupBox();
+            this.l_interface5 = new System.Windows.Forms.Label();
+            this.l_interface4 = new System.Windows.Forms.Label();
+            this.l_interface3 = new System.Windows.Forms.Label();
+            this.tb_result = new System.Windows.Forms.TextBox();
+            this.b_calculate = new System.Windows.Forms.Button();
+            this.l_interface1 = new System.Windows.Forms.Label();
+            this.tb_number2 = new System.Windows.Forms.TextBox();
+            this.lb_NumericSystem = new System.Windows.Forms.ListBox();
+            this.lb_OperationSelection = new System.Windows.Forms.ListBox();
+            this.l_interface6 = new System.Windows.Forms.Label();
+            this.l_interface7 = new System.Windows.Forms.Label();
+            this.gB_interface.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_number1
+            // 
+            this.tb_number1.Enabled = false;
+            this.tb_number1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_number1.Location = new System.Drawing.Point(6, 138);
+            this.tb_number1.MaxLength = 8;
+            this.tb_number1.Multiline = true;
+            this.tb_number1.Name = "tb_number1";
+            this.tb_number1.Size = new System.Drawing.Size(100, 29);
+            this.tb_number1.TabIndex = 0;
+            this.tb_number1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.text_press);
+            // 
+            // gB_interface
+            // 
+            this.gB_interface.Controls.Add(this.l_interface7);
+            this.gB_interface.Controls.Add(this.l_interface6);
+            this.gB_interface.Controls.Add(this.lb_OperationSelection);
+            this.gB_interface.Controls.Add(this.lb_NumericSystem);
+            this.gB_interface.Controls.Add(this.l_interface5);
+            this.gB_interface.Controls.Add(this.l_interface4);
+            this.gB_interface.Controls.Add(this.l_interface3);
+            this.gB_interface.Controls.Add(this.tb_result);
+            this.gB_interface.Controls.Add(this.b_calculate);
+            this.gB_interface.Controls.Add(this.l_interface1);
+            this.gB_interface.Controls.Add(this.tb_number2);
+            this.gB_interface.Controls.Add(this.tb_number1);
+            this.gB_interface.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gB_interface.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gB_interface.Location = new System.Drawing.Point(0, 0);
+            this.gB_interface.Name = "gB_interface";
+            this.gB_interface.Size = new System.Drawing.Size(872, 467);
+            this.gB_interface.TabIndex = 1;
+            this.gB_interface.TabStop = false;
+            this.gB_interface.Text = "Арифметические операции с числами в позиционной системе счисления";
+            // 
+            // l_interface5
+            // 
+            this.l_interface5.AutoSize = true;
+            this.l_interface5.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_interface5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.l_interface5.Location = new System.Drawing.Point(287, 112);
+            this.l_interface5.Name = "l_interface5";
+            this.l_interface5.Size = new System.Drawing.Size(79, 23);
+            this.l_interface5.TabIndex = 10;
+            this.l_interface5.Text = "Число 2";
+            // 
+            // l_interface4
+            // 
+            this.l_interface4.AutoSize = true;
+            this.l_interface4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_interface4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.l_interface4.Location = new System.Drawing.Point(6, 110);
+            this.l_interface4.Name = "l_interface4";
+            this.l_interface4.Size = new System.Drawing.Size(79, 23);
+            this.l_interface4.TabIndex = 9;
+            this.l_interface4.Text = "Число 1";
+            // 
+            // l_interface3
+            // 
+            this.l_interface3.AutoSize = true;
+            this.l_interface3.Location = new System.Drawing.Point(5, 230);
+            this.l_interface3.Name = "l_interface3";
+            this.l_interface3.Size = new System.Drawing.Size(97, 25);
+            this.l_interface3.TabIndex = 8;
+            this.l_interface3.Text = "Решение";
+            // 
+            // tb_result
+            // 
+            this.tb_result.Location = new System.Drawing.Point(4, 258);
+            this.tb_result.Multiline = true;
+            this.tb_result.Name = "tb_result";
+            this.tb_result.Size = new System.Drawing.Size(856, 203);
+            this.tb_result.TabIndex = 7;
+            this.tb_result.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Tb_result_KeyPress);
+            // 
+            // b_calculate
+            // 
+            this.b_calculate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b_calculate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.b_calculate.Location = new System.Drawing.Point(374, 129);
+            this.b_calculate.Name = "b_calculate";
+            this.b_calculate.Size = new System.Drawing.Size(129, 38);
+            this.b_calculate.TabIndex = 6;
+            this.b_calculate.Text = "Вычислить";
+            this.b_calculate.UseVisualStyleBackColor = true;
+            this.b_calculate.MouseDown += new System.Windows.Forms.MouseEventHandler(this.B_calculate_MouseDown);
+            // 
+            // l_interface1
+            // 
+            this.l_interface1.AutoSize = true;
+            this.l_interface1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_interface1.Location = new System.Drawing.Point(7, 38);
+            this.l_interface1.Name = "l_interface1";
+            this.l_interface1.Size = new System.Drawing.Size(377, 23);
+            this.l_interface1.TabIndex = 3;
+            this.l_interface1.Text = "Сначало выберите систему для рассчета:";
+            // 
+            // tb_number2
+            // 
+            this.tb_number2.Enabled = false;
+            this.tb_number2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_number2.Location = new System.Drawing.Point(268, 138);
+            this.tb_number2.MaxLength = 8;
+            this.tb_number2.Multiline = true;
+            this.tb_number2.Name = "tb_number2";
+            this.tb_number2.Size = new System.Drawing.Size(100, 29);
+            this.tb_number2.TabIndex = 2;
+            this.tb_number2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.text_press);
+            // 
+            // lb_NumericSystem
+            // 
+            this.lb_NumericSystem.FormattingEnabled = true;
+            this.lb_NumericSystem.ItemHeight = 25;
+            this.lb_NumericSystem.Items.AddRange(new object[] {
+            "2 - я",
+            "8 - я",
+            "10 - я",
+            "16 - ричная"});
+            this.lb_NumericSystem.Location = new System.Drawing.Point(10, 64);
+            this.lb_NumericSystem.Name = "lb_NumericSystem";
+            this.lb_NumericSystem.Size = new System.Drawing.Size(139, 29);
+            this.lb_NumericSystem.TabIndex = 11;
+            this.lb_NumericSystem.SelectedIndexChanged += new System.EventHandler(this.Lb_NumericSystem_SelectedIndexChanged);
+            // 
+            // lb_OperationSelection
+            // 
+            this.lb_OperationSelection.Enabled = false;
+            this.lb_OperationSelection.FormattingEnabled = true;
+            this.lb_OperationSelection.ItemHeight = 25;
+            this.lb_OperationSelection.Items.AddRange(new object[] {
+            "прибавить к",
+            "отнять"});
+            this.lb_OperationSelection.Location = new System.Drawing.Point(112, 138);
+            this.lb_OperationSelection.Name = "lb_OperationSelection";
+            this.lb_OperationSelection.Size = new System.Drawing.Size(150, 29);
+            this.lb_OperationSelection.TabIndex = 12;
+            this.lb_OperationSelection.SelectedIndexChanged += new System.EventHandler(this.Lb_OperationSelection_SelectedIndexChanged);
+            // 
+            // l_interface6
+            // 
+            this.l_interface6.AutoSize = true;
+            this.l_interface6.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_interface6.ForeColor = System.Drawing.Color.Maroon;
+            this.l_interface6.Location = new System.Drawing.Point(12, 198);
+            this.l_interface6.Name = "l_interface6";
+            this.l_interface6.Size = new System.Drawing.Size(804, 23);
+            this.l_interface6.TabIndex = 13;
+            this.l_interface6.Text = "Мы работаем с неотрицательными действительными числами не длиннее 8-ми символов";
+            // 
+            // l_interface7
+            // 
+            this.l_interface7.AutoSize = true;
+            this.l_interface7.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_interface7.ForeColor = System.Drawing.Color.Red;
+            this.l_interface7.Location = new System.Drawing.Point(12, 175);
+            this.l_interface7.Name = "l_interface7";
+            this.l_interface7.Size = new System.Drawing.Size(137, 23);
+            this.l_interface7.TabIndex = 14;
+            this.l_interface7.Text = "Напоминание:";
+            // 
+            // F_ArithmeticOperations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(872, 467);
+            this.Controls.Add(this.gB_interface);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "F_ArithmeticOperations";
+            this.Text = "Арифметические операции";
+            this.gB_interface.ResumeLayout(false);
+            this.gB_interface.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_number1;
+        private System.Windows.Forms.GroupBox gB_interface;
+        private System.Windows.Forms.Label l_interface1;
+        private System.Windows.Forms.TextBox tb_number2;
+        private System.Windows.Forms.Label l_interface3;
+        private System.Windows.Forms.TextBox tb_result;
+        private System.Windows.Forms.Button b_calculate;
+        private System.Windows.Forms.Label l_interface5;
+        private System.Windows.Forms.Label l_interface4;
+        private System.Windows.Forms.ListBox lb_NumericSystem;
+        private System.Windows.Forms.ListBox lb_OperationSelection;
+        private System.Windows.Forms.Label l_interface6;
+        private System.Windows.Forms.Label l_interface7;
+    }
+}
+
---
+++ ArithmeticOperations/ArithmeticOperations/F_ArithmeticOperations.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArithmeticOperations
+{
+    public partial class F_ArithmeticOperations : Form
+    {
+        private int system;
+        public F_ArithmeticOperations()
+        {
+            InitializeComponent();
+        }
+        private void text_press_system(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            int i = (int)c;
+            if (!(i == 8 || Char.IsDigit(c)))
+            {
+                //  запрет ввода любых символов, кроме
+                // цифр и backspace
+                e.Handled = true;
+            }
+        }
+        private void text_press(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            int i = (int)c;
+            int n_syst = lb_NumericSystem.SelectedIndex;
+            // запрет ввода любых символов, кроме
+            // backspace, цифр и букв диапазона A-F (заглавных)
+            if ((n_syst == 3) && (!(i == 8 || (i >= 48 && i <= 57)|| (i >= 65 && i <= 70)))
+                // backspace, запрет любых символов, кроме цифр 0 и 1)
+                || (n_syst == 0) && (!(i == 8 || (i == 48 || i == 49)))
+                // backspace, запрет любых символов, кроме цифр 0 до 7)
+                || (n_syst == 1) && (!(i == 8 || (i >= 48 && i <= 55)))
+                // backspace, запрет любых символов, кроме цифр)
+                || (n_syst == 2) && !(i == 8 || Char.IsDigit(c)))
+            {
+                
+                e.Handled = true;
+            }
+        }
+
+        private void error_handling()
+        {
+            try
+            {
+                if (tb_number1.Text == "" | tb_number2.Text == "" | lb_NumericSystem.SelectedIndex < 0)
+                {
+                    throw new Exception("Внимание! Проверьте, пожалуйста, " +
+                        "обязательное для заполнения поле (одно или несколько) не заполнено!");
+                }
+
+                if (lb_OperationSelection.SelectedIndex < 0)
+                {
+                    throw new Exception("Внимание! Не выбран элемент в списке!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                tb_result.Text = ($"Ошибка: {ex.Message}");
+                tb_result.ForeColor = Color.Red;
+            }
+
+        }
+        private void error2_handling()
+        {
+            try
+            {
+                
+            }
+            catch(System.ArgumentOutOfRangeException ex)
+            {
+                tb_result.Text = ($"Ошибка: {ex.Message}");
+                tb_result.ForeColor = Color.Red;
+            }
+            
+        }
+        private void add_one_number_to_another()
+        {
+
+                string number1 = tb_number1.Text;
+                string number2 = tb_number2.Text;
+                int num1 = Convert.ToInt32(number1, system);
+                int num2 = Convert.ToInt32(number2, system);
+                int result = num1 + num2;
+                string result1 = Convert.ToString(result, system);
+                if (lb_NumericSystem.SelectedIndex == 3)
+                {
+                    result1 = result1.ToUpper();
+                }
+
+                tb_result.ForeColor = Color.DarkBlue;
+                tb_result.AppendText(number1 + " + " + number2 + " = " + result1 + Environment.NewLine);
+                tb_result.AppendText("_________________________" + Environment.NewLine);
+                tb_result.AppendText("Ответ: " + result1 + Environment.NewLine);
+           
+
+        }
+
+
+        private void subtract_one_number_from_another()
+        {
+            
+           try
+            {
+                string number1 = tb_number1.Text;
+                string number2 = tb_number2.Text;
+                int num1 = Convert.ToInt32(number1, system);
+                int num2 = Convert.ToInt32(number2, system);
+                int result = num1 - num2;
+                if (result < 0) throw new Exception("В настойщее время программа " +
+                   "не поддерживает отрицательные числа, " +
+                   "а результатом вычитания станет отрицательное число."); 
+                string result1 = Convert.ToString(result, system);
+                if (lb_NumericSystem.SelectedIndex == 3)
+                {
+                    result1 = result1.ToUpper();
+                }
+                tb_result.ForeColor = Color.DarkBlue;
+                tb_result.AppendText(number1 + " - " + number2 + " = " + result1 + Environment.NewLine);
+                tb_result.AppendText("_________________________" + Environment.NewLine);
+                tb_result.AppendText("Ответ: " + result1 + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                tb_result.Text = ($"Ошибка: {ex.Message}");
+                tb_result.ForeColor = Color.Red;
+            }
+        }
+
+        private void B_calculate_MouseDown(object sender, MouseEventArgs e)
+        {
+            tb_result.Clear();
+            error_handling();
+            error2_handling();
+            if (lb_OperationSelection.SelectedIndex == 0)
+            {
+                add_one_number_to_another();
+            }
+
+            if (lb_OperationSelection.SelectedIndex == 1)
+
+            {
+                subtract_one_number_from_another();
+            }
+
+        }
+
+        private void Tb_NumericSystem_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Lb_NumericSystem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lb_NumericSystem.SelectedIndex >= 0)
+            {
+                tb_number1.Enabled = true;
+                lb_OperationSelection.Enabled = true;
+                tb_number2.Enabled = true;
+                tb_number1.Clear();
+                tb_number2.Clear();
+
+            }
+
+            switch (lb_NumericSystem.SelectedIndex)
+            {
+                case 0:
+                    tb_result.Text = "Выбрана 2-ва ичная система счисления";
+                    system = 2;
+                    break;
+                case 1:
+                    tb_result.Text = "Выбрана 8-ми ричная система счисления ";
+                    system = 8;
+                    break;
+                case 2:
+                    tb_result.Text = "Выбрана 10-ти ричная система счисления ";
+                    system = 10;
+                    break;
+                case 3:
+                    tb_result.Text = "Выбрана 16-ти ричная система счисления ";
+                    system = 16;
+                    break;
+
+            }
+        }
+
+        private void Lb_OperationSelection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (lb_OperationSelection.SelectedIndex)
+            {
+                case 0:
+                    tb_result.Text = "Выбрана оперция сложения";
+                    break;
+                case 1:
+                    tb_result.Text = "Выбрана операция вычитания";
+                    break;
+            }
+         }
+
+        private void Tb_result_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+    }
+
